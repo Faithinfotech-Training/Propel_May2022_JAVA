97c03428f626366940d87fc503fc4ae556ec58f1
pl1961akhilarakesh <akhilaunni631@gmail.com>
2022-08-25 15:15:48 +0530

    changes made sql

--- ViewModel/MedicineBillViewModel.cs
+++ ViewModel/MedicineBillViewModel.cs
@@ -1,14 +1,20 @@
-﻿namespace CMSByTeamJava.ViewModel
+﻿using System;
+
+namespace CMSByTeamJava.ViewModel
 {
     public class MedicineBillViewModel
     {
+        public DateTime? CreatedDate { get; set; }
+        public string PatientName { get; set; }
+        public string StaffName { get; set; }
+        public int? DoctorId { get; set; }
         public string MedicineName { get; set; }
         public int? MedicineAmount { get; set; }
 
-        public int? DoctorId { get; set; }
         public int? MedicineTimingId { get; set; }
         public string Course { get; set; }
 
 
     }
 }
+
