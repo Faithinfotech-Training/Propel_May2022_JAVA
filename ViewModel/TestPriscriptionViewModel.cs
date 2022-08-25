180dcd4a8a5cb5fa59ac2e6d5f9622c9188b8abe
jerishnixon30@gmail.com <jerishnixon30@gmail.com>
2022-08-25 02:22:52 -0700

    test

---
+++ ViewModel/TestPriscriptionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSByTeamJava.ViewModel
+{
+    public class TestPriscriptionViewModel
+    {
+        public int PatientId { get; set; }
+        public string PatientName { get; set; }
+    }
+}
